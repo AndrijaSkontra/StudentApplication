9714627c2818661e7484cb459279bcd185e044a6
AndrijaSkontra <askontra22@student.unizd.hr>
2024-03-29 08:11:39 +0100

    add bootstrap look to tables and buttons

--- frontend/src/app/ingredients-table/ingredients-table.component.ts
+++ frontend/src/app/ingredients-table/ingredients-table.component.ts
@@ -1,11 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {Ingredient} from "../models/Ingredient";
 import {IngredientService} from "../services/ingredient.service";
+import {DecimalPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-ingredients-table',
   standalone: true,
-  imports: [],
+    imports: [
+        DecimalPipe,
+        NgForOf
+    ],
   templateUrl: './ingredients-table.component.html',
   styleUrl: './ingredients-table.component.scss'
 })
